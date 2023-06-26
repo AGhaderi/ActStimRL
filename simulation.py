b33252c7f682bc72100de779f1477df03e8aab21
Amin Ghaderi <amin.g.ghaderi@gmail.com>
2023-06-26 13:10:32 +0200

    hierarchical True Parameters

--- simulation.py
+++ simulation.py
@@ -4,191 +4,150 @@ import pandas as pd
 from scipy.io import loadmat
 import os
 
+def trueParam(alphaAct_mu, alphaAct_sd,
+              alphaClr_mu, alphaClr_sd,
+              weghtAct_mu, weghtAct_sd,
+              beta_mu, beta_sd, simNumber = 1):
+    # List of subjects
+    subList = ['sub-004', 'sub-020', 'sub-012', 'sub-020', 'sub-025', 'sub-026', 'sub-029',
+               'sub-030', 'sub-033', 'sub-034', 'sub-036', 'sub-040', 'sub-041', 'sub-042',
+               'sub-045', 'sub-047', 'sub-048', 'sub-052', 'sub-054', 'sub-056', 'sub-059',
+               'sub-060', 'sub-064', 'sub-065', 'sub-067', 'sub-069', 'sub-070', 'sub-071',
+               'sub-074', 'sub-075', 'sub-076', 'sub-077', 'sub-078', 'sub-079', 'sub-080',
+               'sub-081', 'sub-082', 'sub-083', 'sub-085', 'sub-087', 'sub-088', 'sub-089',
+               'sub-090', 'sub-092', 'sub-108', 'sub-109']
 
-def sample_Bernouli(theta = .5, n_samples = 1):
-    """
-    Generating samples at random from Bernouli density funtion
-    """
-    return (np.random.rand(n_samples) <= theta).astype(int)
+    # Set true parameters for each session and conditions realted to unkown parameters
+    for subName in subList:
 
-def task_design_act_stim(subName ='sim-sub-001'):
-    """
-    This function performs the task desgin for Action an Stimulus Values learning
-    as Probabilistic Reiforcement Learning task to test the relative contribution of
-    Action and Rewarding mechanisms of Dopemiergic system in Parkinson disease.
-    The experimental task design was predefined and stored in .mat files. Also, it is the same for all participants.
-    There are two .m files, first is related to Action value learnig, when it is presented at the begining.
-                            another is related to Stimulus value leanring, when it is presented at the begining.
+        # Get the partisipant's task design from the original behavioral dataset 'originalfMRIbehFiles'
+        task_design = getTaskDesign(subName=subName)
+        # Put true parameters into the task design
+        task_desin_parameters = hierTrueParam(task_design = task_design,
+                                              alphaAct_mu = alphaAct_mu, alphaAct_sd = alphaAct_sd,
+                                              alphaClr_mu = alphaClr_mu, alphaClr_sd = alphaClr_sd,
+                                              weghtAct_mu = weghtAct_mu, weghtAct_sd = weghtAct_sd,
+                                              beta_mu = beta_mu, beta_sd = beta_sd) 
+        # Save task design plus true parameters for each participant
+        parent_dir  = '../data/simulation/'
+        if not os.path.isdir(parent_dir + subName):
+            os.mkdir(parent_dir + subName) 
+
+        if not os.path.isdir(parent_dir + subName + '/' + str(simNumber)):
+            os.mkdir(parent_dir + subName + '/' + str(simNumber))
+            
+        task_desin_parameters.to_csv(parent_dir + subName + '/' + str(simNumber) + '/' +subName +'-task-design-true-param.csv', index=False)
+        
+    # Save hierarchical true parameters
+    dicHierMeanStdParam= ({'label':['Act-session1', 'Stim-session1', 'Act-session2', 'Stim-session2'],
+                           'hierAlphaAct_mu':alphaAct_mu.flatten(),
+                           'hierAlphaAct_sd': alphaAct_sd.flatten(),
+                           'hierAlphaClr_mu': alphaClr_mu.flatten(),
+                           'hierAlphaClr_sd': alphaClr_sd.flatten(),
+                           'hierWeghtAct_mu': weghtAct_mu.flatten(),
+                           'hieWeghtAct_sd': weghtAct_sd.flatten(),
+                           'hierbeta_mu': beta_mu.flatten(),
+                           'hieBeta_sd': beta_sd.flatten()})
+    dataHierMeanStdParam = pd.DataFrame(dicHierMeanStdParam)
     
-    Arguments
-    ----------
-    firstAct: bool
-        True if the task starts with with Action-value, False if the task sarts with Stimulus-value learning
-        
-    Output
-    -------
-    """
-        
-        
-    """The action value learning is the first condition"""
-    dataActFirst = loadmat('../data/predefined-task-design/ExpStruct_ActFirst_winOnly.mat')  
-          
-    # Two sessions, each session contains two runs
-    session = np.repeat(1, 4*42)
+    if not os.path.isdir(parent_dir + 'hierParam'):
+        os.mkdir(parent_dir + 'hierParam') 
 
-    # Four runs, each run contains two blocks (conditions)
-    run = np.concatenate([np.repeat(1, 2*42), #The session 1 and run 1
-                          np.repeat(2, 2*42)])  #The session 1 and run 2
-
-    # the number of trials for eigh conditions, each condition (Action value or Stimulus Value) includes 42 trials
-    trialNumber = np.repeat(np.arange(1, 85), 2)
+    if not os.path.isdir(parent_dir + 'hierParam' + '/' + str(simNumber)):
+        os.mkdir(parent_dir + 'hierParam' + '/' + str(simNumber))
+            
+    dataHierMeanStdParam.to_csv(parent_dir + 'hierParam' + '/' + str(simNumber) + '/hier-Mean-Std-True-Param.csv', index=False)
     
-    # not perfectly counter-balance ledft and right stimulus response            
-    yellowOnLeftSide = sample_Bernouli(n_samples=4*42)
-
-    # not perfectly counter-balance ledft and right action response
-    leftCanBePushed = sample_Bernouli(n_samples=4*42) 
+def getTaskDesign(subName = 'sub-092'):
+    """Extract task design dataframe from a participant"""
+                   
+    # List of existing .csv files for each session and run realted to the subject
+    files = ['/ses-02achieva7t/' + subName + '_ses-02achieva7t_task-DA_run-1_beh.csv',
+             '/ses-02achieva7t/' + subName + '_ses-02achieva7t_task-DA_run-2_beh.csv',
+             '/ses-03achieva7t/' + subName + '_ses-03achieva7t_task-DA_run-1_beh.csv',
+             '/ses-03achieva7t/' + subName + '_ses-03achieva7t_task-DA_run-2_beh.csv']
+    # Main directory of the subject
+    subMainDirec = '../data/originalfMRIbehFiles/'
+    # Making empty Dataframe to be concatenated in all four .csv file of the subject
+    data = pd.DataFrame([])
+    for i in range(len(files)):
+        dirc = subMainDirec + subName + files[i]
+        df = pd.read_csv(dirc)
+        data = pd.concat([data, df])
+    # Remove anu spance between column name              
+    data.columns = data.columns.str.replace(' ', '')
+    # Extract predefined task design     
+    task_design = data[['session', 'run', 'stimActFirst', 'block', 'stimActBlock', 'trialNumber', 'yellowOnLeftSide', 'leftCanBePushed', 'winAmtLeft', 'winAmtRight', 'winAmtYellow', 'winAmtBlue', 'winAmtPushable', 'winAmtPullable', 'yellowCorrect', 'pushCorrect']]
                 
-    # Each block is Action value or Stimulus Value condition
-    block = np.concatenate([np.repeat('Act', 42),  np.repeat('Stim', 42),
-                            np.repeat('Stim', 42), np.repeat('Act', 42)])
+    return task_design
+
+
+def hierTrueParam(task_design,
+                alphaAct_mu, alphaAct_sd,
+                alphaClr_mu, alphaClr_sd,
+                weghtAct_mu, weghtAct_sd,
+                beta_mu, beta_sd): 
     
-    stimActFirst =  np.repeat('Act', 4*42)
+    """
+    Set true parameters in each condition and session independently.
+
+
+    Arguments: 
+        task_design: Dataframe
+        alphaAct_mu: float Mean of learnig rate for action leaning rate
+        alphaAct_sd: STD of learnig rate for action leaning rate
+        ....
         
-         
-    # Announce choice correct for push and Yellow
-    pushCorrect = np.zeros(4*42).astype(int)
-    yellowCorrect = np.zeros(4*42).astype(int)
+    ---------------------------
+    output: Dataframe
     
-    # predefined Yellow correct responce
-    pushCorrect = np.concatenate([dataActFirst['triallist1_1'][0],       # The condition 1 and run 1
-                                  sample_Bernouli(n_samples=42), # The condition 1 and run 2
-                                  sample_Bernouli(n_samples=42), # The condition 2 and run 1
-                                  dataActFirst['triallist2_2'][0]])      # The condition 2 and run 2
-    # predefined Yellow correct responce
-    yellowCorrect = np.concatenate([sample_Bernouli(n_samples=42), 
-                                    dataActFirst['triallist1_2'][0],       
-                                    dataActFirst['triallist2_1'][0],        
-                                    sample_Bernouli(n_samples=42)])
+        Dataframe containing the original task design plus four new colums related to four unkown parameters:
 
-    # winning reward for left and right side
-    winAmtLeft = np.concatenate([leftCanBePushed[0*42:1*42]*dataActFirst['AmtList1_1'][0]+(1-leftCanBePushed[0*42:1*42])*(100-dataActFirst['AmtList1_1'][0]),
-                                 yellowOnLeftSide[1*42:2*42]*dataActFirst['AmtList1_2'][0]+(1-yellowOnLeftSide[1*42:2*42])*(100-dataActFirst['AmtList1_2'][0]),
-                                 leftCanBePushed[2*42:3*42]*dataActFirst['AmtList2_1'][0]+(1-leftCanBePushed[2*42:3*42])*(100-dataActFirst['AmtList1_1'][0]),
-                                 yellowOnLeftSide[3*42:4*42]*dataActFirst['AmtList2_2'][0]+(1-yellowOnLeftSide[3*42:4*42])*(100-dataActFirst['AmtList1_2'][0])])           
-    winAmtRight = 100 - winAmtLeft
+        True values of unkown parameters are generated from truncated normal distributions
 
-    # winning amounts for Cplor respose
-    winAmtYellow = yellowOnLeftSide*winAmtLeft + (1 - yellowOnLeftSide)*winAmtRight
-    winAmtBlue = 100 - winAmtYellow     
+        alphaAct ~ normal(alphaAct_mu, alphaAct_sd) T(0,1)
 
-    # winning amounts for pushed respose 
-    winAmtPushable = leftCanBePushed*winAmtLeft + (1 - leftCanBePushed)*winAmtRight
-    winAmtPullable = 100 - winAmtPushable
+        alphaClr ~ normal(alphaClr_mu, alphaClr_sd) T(0,1).
 
-    # Dictionary of task desing generated by computer
-    dataActDic = ({'session':session,
-                    'run':run,
-                    'stimActFirst':stimActFirst,
-                    'block':block,
-                    'trialNumber':trialNumber,
-                    'yellowOnLeftSide':yellowOnLeftSide,
-                    'leftCanBePushed':leftCanBePushed,
-                    'winAmtLeft':winAmtLeft,
-                    'winAmtRight':winAmtRight,
-                    'winAmtYellow':winAmtYellow,
-                    'winAmtBlue':winAmtBlue,
-                    'winAmtPushable':winAmtPushable,
-                    'winAmtPullable':winAmtPullable,
-                    'pushCorrect':pushCorrect,
-                    'yellowCorrect':yellowCorrect})
-    dataAct = pd.DataFrame(dataActDic)  
+        weghtAct ~ normal(weghtAct_mu, weghtAct_sd) T(0,1).
+
+        beta ~ normal(beta_mu, beta_sd) T(0,10].
     
+    """
     
-    """The stimulus value learning is the first condition"""
-    dataStimFirst = loadmat('../data/predefined-task-design/ExpStruct_StimFirst_winOnly.mat')
-          
-    # Two sessions, each session contains two runs
-    session = np.repeat(2, 4*42)
-
-    # Four runs, each run contains two blocks (conditions)
-    run = np.concatenate([np.repeat(1, 2*42), #The session 1 and run 1
-                          np.repeat(2, 2*42)])  #The session 1 and run 2
-
-    # the number of trials for eigh conditions, each condition (Action value or Stimulus Value) includes 42 trials
-    trialNumber = np.repeat(np.arange(1, 85), 2)
+    task_design[['alphaAct', 'alphaClr', 'weghtAct', 'beta']] = ""    
     
-    # not perfectly counter-balance ledft and right stimulus response            
-    yellowOnLeftSide = sample_Bernouli(n_samples=4*42)
-
-    # not perfectly counter-balance ledft and right Stimulus response
-    leftCanBePushed = sample_Bernouli(n_samples=4*42) 
-                
-    # Each block is Action value or Stimulus Value condition
-    block = np.concatenate([np.repeat('Stim', 42),  np.repeat('Act', 42),
-                            np.repeat('Act', 42), np.repeat('Stim', 42)])
-
-    stimActFirst = np.repeat('Stim', 4*42)
-        
-         
-    # Announce choice correct for push and Yellow
-    pushCorrect = np.zeros(4*42).astype(int)
-    yellowCorrect = np.zeros(4*42).astype(int)
+    nses = 2
+    ncond = 2
+    condition = ['Act', 'Stim']
     
-    # predefined Yellow correct responce
-    pushCorrect = np.concatenate([dataStimFirst['triallist1_1'][0],       # The condition 1 and run 1
-                                  sample_Bernouli(n_samples=42), # The condition 1 and run 2
-                                  sample_Bernouli(n_samples=42), # The condition 2 and run 1
-                                  dataStimFirst['triallist2_2'][0]])      # The condition 2 and run 2
-    # predefined Yellow correct responce
-    yellowCorrect = np.concatenate([sample_Bernouli(n_samples=42), 
-                                    dataStimFirst['triallist1_2'][0],       
-                                    dataStimFirst['triallist2_1'][0],        
-                                    sample_Bernouli(n_samples=42)])
-
-    # winning reward for left and right side
-    winAmtLeft = np.concatenate([leftCanBePushed[0*42:1*42]*dataStimFirst['AmtList1_1'][0]+(1-leftCanBePushed[0*42:1*42])*(100-dataStimFirst['AmtList1_1'][0]),
-                                 yellowOnLeftSide[1*42:2*42]*dataStimFirst['AmtList1_2'][0]+(1-yellowOnLeftSide[1*42:2*42])*(100-dataStimFirst['AmtList1_2'][0]),
-                                 yellowOnLeftSide[2*42:3*42]*dataStimFirst['AmtList2_2'][0]+(1-yellowOnLeftSide[2*42:3*42])*(100-dataStimFirst['AmtList2_2'][0]),
-                                 leftCanBePushed[3*42:4*42]*dataStimFirst['AmtList2_1'][0]+(1-leftCanBePushed[3*42:4*42])*(100-dataStimFirst['AmtList2_2'][0])])           
-    winAmtRight = 100 - winAmtLeft
-
-    # winning amounts for Cplor respose
-    winAmtYellow = yellowOnLeftSide*winAmtLeft + (1 - yellowOnLeftSide)*winAmtRight
-    winAmtBlue = 100 - winAmtYellow     
-
-    # winning amounts for pushed respose 
-    winAmtPushable = leftCanBePushed*winAmtLeft + (1 - leftCanBePushed)*winAmtRight
-    winAmtPullable = 100 - winAmtPushable
-
-    # Dictionary of task desing generated by computer
-    dataStimDic = ({'session':session,
-                    'run':run,
-                    'stimActFirst':stimActFirst,
-                    'block':block,
-                    'trialNumber':trialNumber,
-                    'yellowOnLeftSide':yellowOnLeftSide,
-                    'leftCanBePushed':leftCanBePushed,
-                    'winAmtLeft':winAmtLeft,
-                    'winAmtRight':winAmtRight,
-                    'winAmtYellow':winAmtYellow,
-                    'winAmtBlue':winAmtBlue,
-                    'winAmtPushable':winAmtPushable,
-                    'winAmtPullable':winAmtPullable,
-                    'pushCorrect':pushCorrect,
-                    'yellowCorrect':yellowCorrect})
-    
-    dataStim = pd.DataFrame(dataStimDic)
-    
-    
-    # Dataframe of output
-    output = pd.concat([dataAct, dataStim])
-    
-    # save task design
-    parent_dir  = '../data/simulation/'
-    if not os.path.isdir(parent_dir + subName):
-        os.mkdir(parent_dir + subName)    
-    output.to_csv('../data/simulation/' + subName +'/' +subName +'-task-design.csv', index=False)
-    
-    
-    return output
+    # Get random sample for each unkhown parameters from a truncated normal distribution
+    # True parameters are generated for each conition and session independently
+    for s in range(nses):
+        for c in range(ncond):
+            while (True):
+                alphaAct = np.round(np.random.normal(alphaAct_mu[s, c], alphaAct_sd[s, c]), 2)
+                if alphaAct > 0 and alphaAct < 1:
+                    break
+                    
+            while (True):
+                alphaClr = np.round(np.random.normal(alphaClr_mu[s, c], alphaClr_sd[s, c]), 2)
+                if alphaClr > 0 and alphaClr < 1:
+                    break
+            
+            while (True):
+                weghtAct = np.round(np.random.normal(weghtAct_mu[s, c], weghtAct_sd[s, c]), 2)
+                if weghtAct > 0 and weghtAct < 1:
+                    break
+            
+            while (True):
+                beta = np.round(np.random.normal(beta_mu[s, c], beta_sd[s, c]), 2)
+                if beta > 0 and beta <= 10:
+                    break
+            # Put generated true parameters within the origonal task design dataframe
+            task_design.loc[(task_design['session'] == s+1) & (task_design['block'] == condition[c]), 'alphaAct'] = alphaAct
+            task_design.loc[(task_design['session'] == s+1) & (task_design['block'] == condition[c]), 'alphaClr'] = alphaClr
+            task_design.loc[(task_design['session'] == s+1) & (task_design['block'] == condition[c]), 'weghtAct'] = weghtAct
+            task_design.loc[(task_design['session'] == s+1) & (task_design['block'] == condition[c]), 'beta'] = beta      
+                    
+    return task_design
