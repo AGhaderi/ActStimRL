b7f23ce1127f2e97a41f35b4897bfa731d5bf17e
Amin Ghaderi <amin.g.ghaderi@gmail.com>
2023-12-14 17:01:17 +0100

    model rl

--- Model/run_models/RL_simple.py
+++ Model/run_models/RL_simple.py
@@ -6,8 +6,9 @@ import pandas as pd
 import stan
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle
-import utils
+import sys
+sys.path.append('/mrhome/amingk/Documents/7TPD/ActStimRL')
+from Madule import utils
 import nest_asyncio
 
 # List of subjects
@@ -18,114 +19,116 @@ subList = ['sub-004', 'sub-010', 'sub-012', 'sub-025', 'sub-026', 'sub-029', 'su
            'sub-074', 'sub-075', 'sub-076', 'sub-077', 'sub-078', 'sub-079', 'sub-080', 
            'sub-081', 'sub-082', 'sub-083', 'sub-085', 'sub-087', 'sub-088', 'sub-089', 
            'sub-090', 'sub-092', 'sub-108', 'sub-109']
-sub = subList[1]
+
 # If you want to model fit or just recall ex model fit
 modelFit = True
 # Number of chains in MCMC procedure
-n_chains = 10
+n_chains = 3
 # The number of iteration or samples for each chain in MCM procedure
-n_samples=6000
+n_samples=3000
 # Name of subject
-subName = sub
+subName = subList[0]
 # Main directory of the subject
-subMainDirec = 'data/originalfMRIbehFiles/'
+subMainDirec = '/mnt/projects/7TPD/bids/derivatives/fMRI_DA/data_BehModel/originalfMRIbehFiles/'
 
 # read collected data across data
 behAll = pd.read_csv('/mnt/projects/7TPD/bids/derivatives/fMRI_DA/data_BehModel/originalfMRIbehFiles/AllBehData/behAll.csv')
-# read the specific participant
-behAll_sub = behAll[behAll['sub_ID']==sub]  
+behAll = behAll.replace('Stim', 'Clr')
 
-# The adrees name of pickle file
-pickelDir = subMainDirec + subName + '/' + subName +'_RL_simple.pkl'
-if modelFit == True: 
-    """Fitting data to model and then save as pickle file in the subject directory if modelFit = True"""
-    # Put required data for stan model
-    dataStan = {'N':int(behAll_sub.shape[0]),  
-                'nCond':2, 
-                'nSes':2, 
-                'pushed':np.array(behAll_sub.pushed).astype(int),  
-                'yellowChosen':np.array(behAll_sub.yellowChosen).astype(int), 
-                'winAmtPushable':np.array(behAll_sub.winAmtPushable).astype(int), 
-                'winAmtYellow':np.array(behAll_sub.winAmtYellow).astype(int), 
-                'rewarded':np.array(behAll_sub.correctChoice).astype(int),       
-                'session':np.array(behAll_sub.session).astype(int), 
-                'cond':np.array(behAll_sub.block.replace('Act',1).replace('Stim',2)).astype(int),  
-                'p_push_init':.5, 
-                'p_yell_init':.5}
+# read all conditions and sessions data for a specifit participant
+for session in [1, 2]: # session
+    for reverse in [21, 14]: # two distinct environemnt
+        for condition in ['Act', 'Clr']: # condition
+            behAll_sub = behAll[(behAll['sub_ID']==subName)&(behAll['block']==condition)&(behAll['session']==session)&(behAll['reverse']==reverse)]  
+            # The adrees name of pickle file
+            pickelDir = subMainDirec + subName + '/model/indv/' + subName + '_cond-' + str(condition) + '_sess-' + str(session) + '_env-' + str(reverse) + '_StanRL_simple.pkl'
+            if modelFit == True: 
+                """Fitting data to model and then save as pickle file in the subject directory if modelFit = True"""
+                # Put required data for stan model
+                dataStan = {'N':int(behAll_sub.shape[0]),  
+                            'pushed':np.array(behAll_sub.pushed).astype(int),  
+                            'yellowChosen':np.array(behAll_sub.yellowChosen).astype(int), 
+                            'winAmtPushable':np.array(behAll_sub.winAmtPushable), 
+                            'winAmtPullable':np.array(behAll_sub.winAmtPullable),
+                            'winAmtYellow':np.array(behAll_sub.winAmtYellow), 
+                            'winAmtBlue':np.array(behAll_sub.winAmtPullable),
+                            'rewarded':np.array(behAll_sub.correctChoice).astype(int),       
+                            'p_push_init':.5, 
+                            'p_yell_init':.5}
+                # initial sampling
 
-    # Loading the RL Stan Model
-    file_name = 'stan_models/inv_RLActClr.stan' 
-    file_read = open(file_name, 'r')
-    stan_model = file_read.read()
-    # Use nest-asyncio.This package is needed because Jupter Notebook blocks the use of certain asyncio functions
-    nest_asyncio.apply()
-    # Building Stan Model realted to our proposed model
-    posterior = stan.build(stan_model, data = dataStan)
-    # Start for taking samples from parameters in the Stan Model
-    fit = posterior.sample(num_chains=n_chains, num_samples=n_samples)
+                initials = [] 
+                for c in range(0, n_chains):
+                    chaininit = {
+                        'alphaAct': np.random.uniform(.1, .5),
+                        'alphClr': np.random.uniform(.1, .5),        
+                        'weightAct': np.random.uniform(.1, .5),
+                        'sensitivity': np.random.uniform(.1, .5)
+                    }
+                    initials.append(chaininit)   
 
-    # Save Model Fit
-    utils.to_pickle(stan_fit=fit, save_path = pickelDir)
-else:
-    """Loading the pickle file of model fit from the subject directory if modelFit = False"""
-    loadPkl = utils.load_pickle(load_path=pickelDir)
-    fit = loadPkl['fit']
+                # Loading the RL Stan Model
+                file_name = '../stan_models/RL_simple.stan' 
+                file_read = open(file_name, 'r')
+                stan_model = file_read.read()
+                # Use nest-asyncio.This package is needed because Jupter Notebook blocks the use of certain asyncio functions
+                nest_asyncio.apply()
+                # Building Stan Model realted to our proposed model
+                posterior = stan.build(stan_model, data = dataStan)
+                # Start for taking samples from parameters in the Stan Model
+                fit = posterior.sample(num_chains=n_chains, num_samples=n_samples, init=initials)
 
-# Extracting posterior distributions for each of four main unkhown parameters
-alphaAct_ = fit["alphaAct_"] 
-alphaClr_ = fit["alphaClr_"] 
-weightAct_ = fit["weightAct_"] 
-beta_ = fit["beta_"] 
+                # Save Model Fit
+                utils.to_pickle(stan_fit=fit, save_path = pickelDir)
+            else:
+                """Loading the pickle file of model fit from the subject directory if modelFit = False"""
+                loadPkl = utils.load_pickle(load_path=pickelDir)
+                fit = loadPkl['fit']
 
-# Figure of model fit results in two column and two rows
-fig = plt.figure(figsize=(10, 6), tight_layout=True)
-rows = 2
-columns = 2
+            # Extracting posterior distributions for each of four main unkhown parameters
+            alphaAct_ = fit["alphaAct"].flatten()
+            alphaClr_ = fit["alphaClr"].flatten() 
+            weightAct_ = fit["weightAct"].flatten()
+            beta_ = fit["sensitivity"].flatten() 
 
-# Weghtening
-fig.add_subplot(rows, columns, 1)
-sns.histplot(weightAct_[0, 0], kde=True, stat='density')
-sns.histplot(weightAct_[0, 1], kde=True, stat='density')
-sns.histplot(weightAct_[1, 0], kde=True, stat='density')
-sns.histplot(weightAct_[1, 1], kde=True, stat='density')
-plt.title(subName + ', Weightening', fontsize=12)
-plt.ylabel('Density', fontsize=12)
-plt.xlabel('$w_{(A)}$', fontsize=14)
-plt.legend(['Ses 1, Act', 'Ses 1, Clr', 'Ses 2, Act', 'Ses 2, Clr'], fontsize=8)
+            print(alphaAct_.shape)
+
+            # Figure of model fit results in two column and two rows
+            fig = plt.figure(figsize=(10, 6), tight_layout=True)
+            rows = 2
+            columns = 2
+
+            # Weghtening
+            fig.add_subplot(rows, columns, 1)
+            sns.histplot(weightAct_, kde=True, stat='density')
+            plt.title(subName + ', Weightening', fontsize=12)
+            plt.ylabel('Density', fontsize=12)
+            plt.xlabel('$w_{(A)}$', fontsize=14)
+
+            # Sensitivity
+            fig.add_subplot(rows, columns, 2)
+            sns.histplot(beta_, kde=True, stat='density')
+            sns.histplot(beta_, kde=True, stat='density')
+            plt.title(subName + ', Sensitivity', fontsize=12)
+            plt.ylabel('Density', fontsize=12)
+            plt.xlabel(r'$\beta$', fontsize=14)
 
 
-# Sensitivity
-fig.add_subplot(rows, columns, 2)
-sns.histplot(beta_[0], kde=True, stat='density')
-sns.histplot(beta_[1], kde=True, stat='density')
-plt.title(subName + ', Sensitivity', fontsize=12)
-plt.ylabel('Density', fontsize=12)
-plt.xlabel(r'$\beta$', fontsize=14)
-plt.legend(['Ses 1', 'Ses 2'], fontsize=8)
+            # Action Learning Rate
+            fig.add_subplot(rows, columns, 3)
+            sns.histplot(alphaAct_, kde=True, stat='density')
+            plt.title(subName + ', Action Learning Rate', fontsize=12)
+            plt.ylabel('Density', fontsize=12)
+            plt.xlabel(r'$ \alpha_{(A)} $', fontsize=14)
 
+            # Color Learning Rate
+            fig.add_subplot(rows, columns, 4)
+            sns.histplot(alphaClr_, kde=True, stat='density')
+            plt.title(subName + ', Color Learning Rate', fontsize=12)
+            plt.ylabel('Density', fontsize=12)
+            plt.xlabel(r'$ \alpha_{(C)} $', fontsize=14)
 
-# Action Learning Rate
-fig.add_subplot(rows, columns, 3)
-sns.histplot(alphaAct_[0, 0], kde=True, stat='density')
-sns.histplot(alphaAct_[0, 1], kde=True, stat='density')
-sns.histplot(alphaAct_[1, 0], kde=True, stat='density')
-sns.histplot(alphaAct_[1, 1], kde=True, stat='density')
-plt.title(subName + ', Action Learning Rate', fontsize=12)
-plt.ylabel('Density', fontsize=12)
-plt.xlabel(r'$ \alpha_{(A)} $', fontsize=14)
-plt.legend(['Ses 1, Act', 'Ses 1, Clr', 'Ses 2, Act', 'Ses 2, Clr'], fontsize=8)
+            plt.subplots_adjust(wspace=10.)
 
-# Color Learning Rate
-fig.add_subplot(rows, columns, 4)
-sns.histplot(alphaClr_[0, 0], kde=True, stat='density')
-sns.histplot(alphaClr_[0, 1], kde=True, stat='density')
-sns.histplot(alphaClr_[1, 0], kde=True, stat='density')
-sns.histplot(alphaClr_[1, 1], kde=True, stat='density')
-plt.title(subName + ', Color Learning Rate', fontsize=12)
-plt.ylabel('Density', fontsize=12)
-plt.xlabel(r'$ \alpha_{(C)} $', fontsize=14)
-plt.legend(['Ses 1, Act', 'Ses 1, Clr', 'Ses 2, Act', 'Ses 2, Clr'], fontsize=8)
-
-plt.subplots_adjust(wspace=10.)
-
-fig.savefig(subMainDirec + subName + '/' + subName +'_inv_rlActClr.png', dpi=300)
+            # Save figure of parameter distribution 
+            fig.savefig(subMainDirec + subName + '/model/indv/' + subName + '_cond-' + str(condition) + '_sess-' + str(session) + '_env-' + str(reverse) + '_StanRL_simple.png', dpi=300)
